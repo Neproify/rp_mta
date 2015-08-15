40e7ec943276e3621561186cc20d744e28424886
Neproify <yamasaki3214@gmail.com>
2015-08-15 15:40:51 +0200

    Poprawka elementu gracza wysyłającego wiadomość.

--- resources/chat/chat_s.lua
+++ resources/chat/chat_s.lua
@@ -1,15 +1,15 @@
 addEventHandler("onPlayerChat", root, function(msg, msgType)
 	cancelEvent()
-	local charInfo = client:getData("charInfo")
+	local charInfo = source:getData("charInfo")
 	if not charInfo then
 		return
 	end
 	if msgType == 0 then -- normalny chat
-		local posX, posY, posZ = client.position
+		local posX, posY, posZ = source.position
 		local chatSphere = Colshape.Sphere(posX, posY, posZ, 15)
 		local nearbyPlayers = chatSphere:getElementsWithin("player")
 		for i,v in ipairs(nearbyPlayers) do
-			local name = client.name
+			local name = source.name
 			outputChatBox(name.." mówi: "..msg, v)
 		end
 	end

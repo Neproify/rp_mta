22e4d87187d34213ab76685efcdf5d21c29261b7
Neproify <yamasaki3214@gmail.com>
2015-08-15 15:02:51 +0200

    Kolejna poprawka.

--- resources/player/spawn_s.lua
+++ resources/player/spawn_s.lua
@@ -1,13 +1,9 @@
 addEvent("spawnPlayer", true)
 addEventHandler("spawnPlayer", root, function()
-	local globalInfo = client:getData("globalInfo")
 	local charInfo = client:getData("charInfo")
-	if not globalInfo or not charInfo then
+	if not charInfo then
 		return
 	end
-	--[[spawnPlayer(client, 1481.8495, -1687.1045, 14.0469, 178.7321)
-	fadeCamera(client, true)
-	setCameraTarget(client, client)--]]
 	client:spawn(1481.8495, -1687.1045, 14.0469, 178.7321)
 	client:fadeCamera(true)
 	client:setCameraTarget(client)

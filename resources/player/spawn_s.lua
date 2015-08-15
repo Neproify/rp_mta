4236574cc1b2dbc9c36d043d953e301972fb5453
Neproify <yamasaki3214@gmail.com>
2015-08-16 00:07:42 +0200

    Wczytywanie skina, ustawianie pieniędzy przy spawnie.

--- resources/player/spawn_s.lua
+++ resources/player/spawn_s.lua
@@ -4,7 +4,8 @@ addEventHandler("spawnPlayer", root, function()
 	if not charInfo then
 		return
 	end
-	client:spawn(1481.8495, -1687.1045, 14.0469, 178.7321)
+	client:spawn(1481.8495, -1687.1045, 14.0469, 178.7321, charInfo["skin"])
+	client:setMoney(charInfo["money"], true)
 	client:fadeCamera(true)
 	client:setCameraTarget(client)
 	client:setData("spawned", true)

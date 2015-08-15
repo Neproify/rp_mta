bb85a30eb2d9887bf2e30c2b3792395247a8df08
Neproify <yamasaki3214@gmail.com>
2015-08-15 16:39:21 +0200

    Chat ooc.

--- resources/chat/chat_s.lua
+++ resources/chat/chat_s.lua
@@ -1,12 +1,20 @@
 addEventHandler("onPlayerChat", root, function(msg, msgType)
 	cancelEvent()
 	local charInfo = source:getData("charInfo")
+	local globalInfo = source:getData("globalInfo")
 	if not charInfo then
 		return
 	end
 	if msgType == 0 then -- normalny chat
-		if string.sub(msg, 1, 1) == "." then
-			source:outputChat("Chat ooc")
+		if string.sub(msg, 1, 1) == "." then -- chat ooc
+			local pos = source.position
+			local chatSphere = ColShape.Sphere(pos, 15)
+			local nearbyPlayers = chatSphere:getElementsWithin("player")
+			local oocMsg = string.sub(msg, 2)
+			for i,v in ipairs(nearbyPlayers) do
+				local name = globalInfo["name"]
+				v:outputChat("#FFFFFF(("..name..": "..oocMsg.."))", 0, 0, 0, true)
+			end
 			return
 		end
 		local pos = source.position

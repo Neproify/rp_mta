e2d9bea4e2241c4e23610ff6c3ac981583e6a323
Neproify <yamasaki3214@gmail.com>
2015-08-15 15:40:02 +0200

    Chat IC

--- resources/chat/chat_s.lua
+++ resources/chat/chat_s.lua
@@ -1,3 +1,16 @@
 addEventHandler("onPlayerChat", root, function(msg, msgType)
 	cancelEvent()
+	local charInfo = client:getData("charInfo")
+	if not charInfo then
+		return
+	end
+	if msgType == 0 then -- normalny chat
+		local posX, posY, posZ = client.position
+		local chatSphere = Colshape.Sphere(posX, posY, posZ, 15)
+		local nearbyPlayers = chatSphere:getElementsWithin("player")
+		for i,v in ipairs(nearbyPlayers) do
+			local name = client.name
+			outputChatBox(name.." mówi: "..msg, v)
+		end
+	end
 end)

3dd3a45abaa24e20da49a5031f47f6f298383bd4
Neproify <yamasaki3214@gmail.com>
2015-08-15 15:42:18 +0200

    Poprawka literówki.

--- resources/chat/chat_s.lua
+++ resources/chat/chat_s.lua
@@ -6,7 +6,7 @@ addEventHandler("onPlayerChat", root, function(msg, msgType)
 	end
 	if msgType == 0 then -- normalny chat
 		local posX, posY, posZ = source.position
-		local chatSphere = Colshape.Sphere(posX, posY, posZ, 15)
+		local chatSphere = ColShape.Sphere(posX, posY, posZ, 15)
 		local nearbyPlayers = chatSphere:getElementsWithin("player")
 		for i,v in ipairs(nearbyPlayers) do
 			local name = source.name

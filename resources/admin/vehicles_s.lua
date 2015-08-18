0e7630a33efdb299a4115155c5a6bfa33ba0327f
Neproify <yamasaki3214@gmail.com>
2015-08-18 15:06:35 +0200

    Literówka.

--- resources/admin/vehicles_s.lua
+++ resources/admin/vehicles_s.lua
@@ -19,7 +19,7 @@ addCommandHandler("apojazd", function(player, cmdname, param1, param2, param3)
 			exports.notification:add(player, "Nie znaleziono pojazdu z UID: ".. param2)
 			return
 		end
-		if tonumber(param3) > 0 or tonumber(param3) > 1000 then
+		if tonumber(param3) < 0 or tonumber(param3) > 1000 then
 			exports.notification:add(player, "Podałeś nieprawidłową liczbę HP.")
 			return
 		end

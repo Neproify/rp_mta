061d692d3558d225f7e788b3fc2f24ffe385e8e0
Neproify <yamasaki3214@gmail.com>
2015-08-19 14:27:29 +0200

    ID gracza na chacie OOC.

--- resources/chat/chat_s.lua
+++ resources/chat/chat_s.lua
@@ -11,9 +11,9 @@ addEventHandler("onPlayerChat", root, function(msg, msgType)
 			local chatSphere = ColShape.Sphere(pos, 15)
 			local nearbyPlayers = chatSphere:getElementsWithin("player")
 			local oocMsg = string.sub(msg, 2)
+			local name = globalInfo["name"]
 			for i,v in ipairs(nearbyPlayers) do
-				local name = globalInfo["name"]
-				v:outputChat("#FFFFFF(("..name..": "..oocMsg.."))", 0, 0, 0, true)
+				v:outputChat("#FFFFFF(("..name.."("..source:getData("ID").."): "..oocMsg.."))", 0, 0, 0, true)
 			end
 			return
 		end
@@ -22,8 +22,8 @@ addEventHandler("onPlayerChat", root, function(msg, msgType)
 		local nearbyPlayers = chatSphere:getElementsWithin("player")
 		msg = string.gsub(msg, "<", "#C2A2DA*")
 		msg = string.gsub(msg, ">", "*#FFFFFF")
+		local name = source.name
 		for i,v in ipairs(nearbyPlayers) do
-			local name = source.name
 			v:outputChat("#FFFFFF"..name.." mówi: "..msg, 0, 0, 0, true)
 		end
 	end

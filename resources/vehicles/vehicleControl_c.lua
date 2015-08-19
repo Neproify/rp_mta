ac612f8bd446903d72cedfda70becfff9b0b99a0
Neproify <yamasaki3214@gmail.com>
2015-08-19 13:39:32 +0200

    Odpalanie silnika.

--- resources/vehicles/vehicleControl_c.lua
+++ resources/vehicles/vehicleControl_c.lua
@@ -1,6 +1,15 @@
 addEventHandler("onClientVehicleEnter", root, function(player, seat)
 	if player == localPlayer then
-		local engineState, lights = source:getData("vehicleEngine"), source:getData("vehicleLights")
+		local engineState, lights = source:getData("vehicleEngine")
 		source:setEngineState(engineState)
+	end
+end)
+
+addCommandHandler("silnik", function()
+	if localPlayer:isInVehicle() then
+		if localPlayer.vehicleSeat == 0 then
+			localPlayer.vehicle:setData("vehicleEngine", not localPlayer.vehicle:getData("vehicleEngine"))
+			localPlayer.vehicle:setEngineState(localPlayer.vehicle:getData("vehicleEngine"))
+		end
 	end
 end)

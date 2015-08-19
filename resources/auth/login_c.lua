51ebd89248d8f8a48bca3d8c86f5de7bdae598d3
Neproify <yamasaki3214@gmail.com>
2015-08-20 00:50:57 +0200

    Testowe logowanie za pomocą CEF.

--- resources/auth/login_c.lua
+++ resources/auth/login_c.lua
@@ -6,6 +6,19 @@ addCommandHandler("zaloguj", function(cmd, login, password)
 	triggerServerEvent("onLoginRequest", localPlayer, login, password)
 end)
 
+local loginWindow = nil
+local sw, sh = guiGetScreenSize()
+
+addEventHandler("onClientResourceStart", resourceRoot, function()
+	loginWindow = Browser(640, 480, true)
+	loginWindow:loadURL("http://mta/local/login.html")
+	addEventHandler("onClientRender", root, renderLoginGUI)
+end)
+
+function renderLoginGUI()
+	dxDrawImage(sw - 320, sh - 240, 640, 480, loginWindow)
+end
+
 addEvent("onLoginResult", true)
 addEventHandler("onLoginResult", root, function(result)
 	exports.notification:add(result.message)

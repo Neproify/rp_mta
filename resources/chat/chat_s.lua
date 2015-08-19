52437f3a2db4576ea4426b2665706b9e27bfa488
Neproify <yamasaki3214@gmail.com>
2015-08-19 13:09:24 +0200

    Kolorowanie akcji na chacie IC.

--- resources/chat/chat_s.lua
+++ resources/chat/chat_s.lua
@@ -20,6 +20,8 @@ addEventHandler("onPlayerChat", root, function(msg, msgType)
 		local pos = source.position
 		local chatSphere = ColShape.Sphere(pos, 15)
 		local nearbyPlayers = chatSphere:getElementsWithin("player")
+		msg = string.gsub(msg, "<", "#C2A2DA*")
+		msg = string.gsub(msg, ">", "*#FFFFFF")
 		for i,v in ipairs(nearbyPlayers) do
 			local name = source.name
 			v:outputChat("#FFFFFF"..name.." mówi: "..msg, 0, 0, 0, true)

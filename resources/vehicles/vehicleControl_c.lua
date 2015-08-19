7c89fa7ae344f89c83c8dd9dfa213b83941a5c71
Neproify <yamasaki3214@gmail.com>
2015-08-19 13:44:12 +0200

    Włączanie świateł.

--- resources/vehicles/vehicleControl_c.lua
+++ resources/vehicles/vehicleControl_c.lua
@@ -6,10 +6,22 @@ addEventHandler("onClientVehicleEnter", root, function(player, seat)
 end)
 
 addCommandHandler("silnik", function()
-	if localPlayer:isInVehicle() then
+	if localPlayer.inVehicle then
 		if localPlayer.vehicleSeat == 0 then
 			localPlayer.vehicle:setData("vehicleEngine", not localPlayer.vehicle:getData("vehicleEngine"))
 			localPlayer.vehicle:setEngineState(localPlayer.vehicle:getData("vehicleEngine"))
+		end
+	end
+end)
+
+addCommandHandler("swiatla", function()
+	if localPlayer.inVehicle then
+		if localPlayer.vehicleSeat == 0 then
+			if localPlayer.vehicle.overrideLights == 1 then
+				localPlayer.vehicle.overrideLights = 2
+			elseif localPlayer.vehicle.overrideLights == 2 then
+				localPlayer.vehicle.overrideLights = 1
+			end
 		end
 	end
 end)

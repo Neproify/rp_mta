a1d7118f2e6e0a997af37e418c3e8cf1ac32b9de
Neproify <yamasaki3214@gmail.com>
2015-08-23 00:34:48 +0200

    Nie ma to jak bezmyślne kopiowanie. ;)

--- resources/playergroups/groupsPanel_c.lua
+++ resources/playergroups/groupsPanel_c.lua
@@ -5,19 +5,19 @@ local sw, sh = guiGetScreenSize()
 addEventHandler("onClientResourceStart", resourceRoot, function()
 	groupsWindow = GuiBrowser(sw /2 - 200, sh /2 - 150, 400, 300, true, true, false)
 	addEventHandler("onClientBrowserCreated", groupsWindow, function()
-		loginWindow:getBrowser():loadURL("http://mta/local/playerGroups.html")
-		loginWindow:getBrowser():setRenderingPaused(true)
+		groupsWindow:getBrowser():loadURL("http://mta/local/playerGroups.html")
+		groupsWindow:getBrowser():setRenderingPaused(true)
 		guiSetVisible(groupsWindow, false)
 	end)
 end)
 
 addCommandHandler("g", function()
 	if panelEnabled then
-		loginWindow:getBrowser():setRenderingPaused(true)
+		groupsWindow:getBrowser():setRenderingPaused(true)
 		guiSetVisible(groupsWindow, false)
 		panelEnabled = false
 	else
-		loginWindow:getBrowser():setRenderingPaused(false)
+		groupsWindow:getBrowser():setRenderingPaused(false)
 		guiSetVisible(groupsWindow, true)
 		panelEnabled = true
 	end
